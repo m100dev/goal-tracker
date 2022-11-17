a8a71b903b7719ecc0e24c7bc792594e30ead571
Marc Bastien <m100.dev@gmail.com>
2022-11-17 11:56:21 -0500

    Updated CourseInput component to use React Styled components to explore setting dynamic css with React Styled components

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-
+//import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
 
+import styles from './CourseInput.module.css';
+
+/*
 const FormControl = styled.div`
   margin: 0.5rem 0;
 
@@ -38,7 +39,7 @@ const FormControl = styled.div`
   &.invalid label {
     color: red;
   }
-`;
+`; */
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -62,12 +63,12 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input 
           type="text" 
           onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
